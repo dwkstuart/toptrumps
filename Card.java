d9bec7b6a9259cbd8b37a734c4076fc40d4e243d
unknown <davidwkstuart@gmail.com>
2017-01-14 14:37:23 +0000

    Card class with constructor and return methods for information

--- Card.java
+++ Card.java
@@ -1,41 +1,49 @@
- public class Card
- {
- 
- private String description;
- private int height, weight, length,  ferocity, intelligence; 
- 
- public Card(String details){
-	String info = details;
-	values[]= info.split(" ");
- 
-	description = info [0];
-	height = Interger.parseInt(info[1]);	
-	weight = Interger.parseInt(info[2]);			
-	length = Interger.parseInt(info[3]);	
-	ferocity = Interger.parseInt(info[4]);	
-	intelligence = Interger.parseInt(info[5]);
- }
- 
- public String getDescription(){
- return description;}
- 
- public int getIntelligence() {
-return intelligence;}
- 
- public int getFerocity(){
- return feocity;}
- 
- public int getLength() {
-return length;}
- 
- public int getHeight() {
-return height;}
- 
- public int getWeight() {
-return weight;}
- 
- public String toString {
-return String card = String.format("The %s has %d height, %d weight, %d length, %d ferocity and %d intelligence", 
-	description, height, weight, length,  ferocity, intelligence); }
- 
+public class Card {
+
+	private String description = "";
+	private int height, weight, length, ferocity, intelligence;
+
+	public Card(String details) {
+		String info = details;
+		String[] values = info.split(" +");
+		// values[]= info.split(" ");
+
+		description = values[0];
+		height = Integer.parseInt(values[1]);
+		weight = Integer.parseInt(values[2]);
+		length = Integer.parseInt(values[3]);
+		ferocity = Integer.parseInt(values[4]);
+		intelligence = Integer.parseInt(values[5]);
+
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public int getIntelligence() {
+		return intelligence;
+	}
+
+	public int getFerocity() {
+		return ferocity;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public String toString()
+	{String card = String.format("The %s has %d height, %d weight, %d length, %d ferocity and %d intelligence", 
+	description, height, weight, length,  ferocity, intelligence);
+		return card; }
+
 }
