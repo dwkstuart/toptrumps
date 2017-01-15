04cf0cdd2c422532c5da81552f9c05b5f8f16818
dwkstuart <davidwkstuart@gmail.com>
2017-01-15 18:35:43 +0000

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,8 +1,14 @@
+/** Class that holds the decriptions and values of a single top trump card
+*/
 public class Card {
 
 	private String description = "";
 	private int height, weight, length, ferocity, intelligence;
-
+	
+	/**@param String details, string passed in with description and values 
+	*	from text file
+	*sets the instance variables
+	*/
 	public Card(String details) {
 		String info = details;
 		String[] values = info.split(" +");
@@ -16,31 +22,45 @@ public class Card {
 		intelligence = Integer.parseInt(values[5]);
 
 	}
-
+	
+	/*@return  return the card description 
+	*/
 	public String getDescription() {
 		return description;
 	}
 
+	/*@return  return the instance vairable intelligence
+	*/
 	public int getIntelligence() {
 		return intelligence;
 	}
 
+	/*@return  return the the instance variable ferocity
+	*/
 	public int getFerocity() {
 		return ferocity;
 	}
 
+	/*@return  return the instance variable length
+	*/
 	public int getLength() {
 		return length;
 	}
 
+	/*@return  return the instance variable height
+	*/
 	public int getHeight() {
 		return height;
 	}
 
+	/*@return  return the instance variable height
+	*/
 	public int getWeight() {
 		return weight;
 	}
 
+	/*@return  return the card description with values
+	*/
 	public String toString()
 	{String card = String.format("The %s has %d height, %d weight, %d length, %d ferocity and %d intelligence", 
 	description, height, weight, length,  ferocity, intelligence);
