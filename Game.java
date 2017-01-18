48404eeb239f448fb833ee305dd351bc66ad29c5
kingdomlevel <nllmrrs@aol.com>
2017-01-18 19:02:23 +0000

    Fixed a bad version of Game.java

--- Game.java
+++ Game.java
@@ -1,11 +1,7 @@
 
 public class Game{
    private int numCurrentPlayers;
-<<<<<<< HEAD
    private Player[] activePlayers;   // array of all the current player objects
-=======
-   private Player[] activePlayers[];   // array of all the current player objects
->>>>>>> 573a70dd7d2ff9096e91935e01fcf02123e4f4e7
 
    private String category;
    private int playerPointer;          // indicates which player's turn it is
@@ -14,11 +10,7 @@ public class Game{
    // constructor
    public Game(int numberOfPlayers) {
       Card[] deck = createDeck();
-<<<<<<< HEAD
       dealCards(deck);
-=======
-      dealCards[deck];
->>>>>>> 573a70dd7d2ff9096e91935e01fcf02123e4f4e7
       numCurrentPlayers = numberOfPlayers;
       activePlayers = new Player[numberOfPlayers];
       roundCount++;
@@ -27,12 +19,10 @@ public class Game{
 
 
    /**
-<<<<<<< HEAD
    * Method to create an array of card objects and subsequently randomise the order
    * of their indexes.
    */
    public Card[] createDeck() {
-      // UNFINISHED
       return null;
    }
 
@@ -41,34 +31,13 @@ public class Game{
    * @param Card[] deckToBeDealt [description]
    */
    public void dealCards(Card[] deckToBeDealt) {
-      // UNFINISHED
+
    }
 
    /**
    * Method to instansiate a new round of Top Trumps, in which it is the user's
    * turn to select a category.
    */
-=======
-    * Method to create an array of card objects and subsequently randomise the order
-    * of their indexes.
-    */
-   public Card[] createDeck() {
-
-   }
-
-   /**
-    * Method to deal the deck out equally between the number of players.
-    * @param Card[] deckToBeDealt [description]
-    */
-   public void dealCards(Card[] deckToBeDealt) {
-
-   }
-
-   /**
-    * Method to instansiate a new round of Top Trumps, in which it is the user's
-    * turn to select a category.
-    */
->>>>>>> 573a70dd7d2ff9096e91935e01fcf02123e4f4e7
    public void createUserRound(String chosenCategory) {
       // case that it is the user's turn to select a category.
       Round currentRound = new Round(activePlayers, chosenCategory);
@@ -76,15 +45,9 @@ public class Game{
       playerPointer++;
    }
    /**
-<<<<<<< HEAD
    * Method to instansiate a new round of Top Trumps, in which it is the computer's
    * turn to select a category.
    */
-=======
-    * Method to instansiate a new round of Top Trumps, in which it is the computer's
-    * turn to select a category.
-    */
->>>>>>> 573a70dd7d2ff9096e91935e01fcf02123e4f4e7
    public void createCPURound() {
       // case that it is a CPU player's turn to select a category.
       Round currentRound = new Round(activePlayers, playerPointer);
