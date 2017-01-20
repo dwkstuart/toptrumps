0858113e0b11acc6d61f829bc4e66ad261149fb3
rashelie <rachel.cunningham787@yahoo.co.uk>
2017-01-20 11:37:50 +0000

    Partially complete
    I still need to do addCard and removeCard. Please feel free to review my code and make sure I've implemented in accordance with design spec

--- Player.java
+++ Player.java
@@ -6,4 +6,69 @@
  
 public class Player {
 
+// Instance variables- player id, an int between 0 and 4
+private int playerID; 
+private int playerNum;
+private int [] Card = new int [40];// Is this what you meant here?
+private String currentCard = "";
+private int numCards;
+
+//constructor
+public Player(int player_ID){
+   playerID = player_ID;
+}
+              
+//a method to check the correct player number is selected
+   public numPlayer(int playerNum) {
+      if (playerNum < 1 || playerNum > 4) {
+         throw new IllegalArgumentException();
+      } else {
+         this.playerNum = playerNum;
+      }
+   }
+
+//a  method to get the number of players selected
+   public int getPlayerNum() {
+      return playerNum;
+   }
+
+//a method to store the number of players selected by the user
+   public void setPlayerNum(int newPlayerNum) {
+    if (newPlayerNum < 0 || newPlayerNum > 100) {
+         throw new IllegalArgumentException();
+      } else {
+      playerNum = newPlayerNum;
+    }
+   }
+// Boolean active - if a player is still in the game or not ?not sure if this is needed
+boolean active = true;
+        if (active) {
+            System.out.println("I'm in the game");
+        }
+        active = false;
+        if (!active) {
+            System.out.println("I'm out of the game");
+        }
+ 
+// getCurrentCard - return the top card  in their hand; at 0 in the array
+public String getCurrentCard() {
+		return currentCard;
+	}
+// addCard; -add a card to the back of their hand, first null value.
+
+// removeCard? - remove top card from the hand and shift array to fill gap at 0
+// (not sure if we need this as well as getCurrentCard)
+
+// getNumCards - return the number of card objects in the array
+public String getNumCards() {
+		return numCards;
+// getID
+public String getPlayerID() {
+		return playerID;
+// getStatus - return boolean if they are an active player or not
+public String getCurrentCard() {
+		return currentCard;
+// setStatus ?might not be needed
+
+
 }
